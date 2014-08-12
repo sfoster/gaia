1ea1a6013757850d9619310c418b1a0596adce17
Sam Foster <sfoster@mozilla.com>
2014-08-11 17:01:55 -0700

    WIP

--- apps/system/js/app_window.js
+++ apps/system/js/app_window.js
@@ -509,13 +509,6 @@
                   '<div class="icon"></div>' +
                   '<span class="title"></span>' +
                 '</div>' +
-                '<menu class="buttonbar">' +
-                    '<button class="close-button" data-button-action="close"' +
-                    '   role="button"></button>' +
-                    '<button class="favorite-button" ' +
-                    '    data-button-action="favorite"' +
-                    '   role="button"></button>' +
-                '</menu>' +
               '</div>' +
               '<div class="fade-overlay"></div>' +
               '<div class="touch-blocker"></div>' +
@@ -676,6 +669,7 @@
     'authDialog': window.AppAuthenticationDialog,
     'contextmenu': window.BrowserContextMenu,
     'childWindowFactory': window.ChildWindowFactory,
+    'taskCard': window.AppTaskCard
   };
 
   /**
