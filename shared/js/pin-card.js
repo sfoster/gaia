c20183b5795e0ab3b2954b45b82afd4b605d52b4
Michael Henretty <michael.henretty@gmail.com>
2015-05-18 17:45:19 -0700

    Updating cards to use linked data

--- shared/js/pin-card.js
+++ shared/js/pin-card.js
@@ -3,14 +3,67 @@
 'use strict';
 
 (function(exports) {
+
+  function getTitle(data) {
+    if (data.title) {
+      return data.title;
+    }
+    if (data.linkedData.title) {
+      return data.linkedData.title;
+    }
+    return data.url;
+  }
+
+  function getDescription(data) {
+    if (data.content) {
+      return data.content;
+    }
+    if (data.linkedData.description) {
+      return data.linkedData.description;
+    }
+    return '';
+  }
+
+  function getBackgroundURL(data) {
+    var img = data.linkedData.image || data.screenshot || data.tile;
+    if (!img) {
+      return null;
+    } else {
+      return (typeof img === 'string') ? img : URL.createObjectURL(img);
+    }
+  }
+
+  function getBrightness(color) {
+    var colorCodes = /rgb\((\d+), (\d+), (\d+)\)/.exec(color);
+    if (!colorCodes || colorCodes.length === 0) {
+      return;
+    }
+    var r = parseInt(colorCodes[1]);
+    var g = parseInt(colorCodes[2]);
+    var b = parseInt(colorCodes[3]);
+    return Math.sqrt((r*r) * 0.241 + (g*g) * 0.691 + (b*b) * 0.068);
+  }
+
   var PinCard = function(cardData) {
+    cardData.linkedData = cardData.linkedData || {};
     var container = document.createElement('div');
     container.classList.add('pin-card');
 
+    var background = document.createElement('div');
+    background.className = 'background';
+    background.style.backgroundColor = cardData.themeColor || 'white';
+    var brightness =
+      getBrightness(window.getComputedStyle(background).backgroundColor);
+    if (brightness < 200) {
+      container.classList.add('light');
+    } else {
+      container.classList.remove('light');
+    }
+
     var header = document.createElement('header');
     var content = document.createElement('p');
-    header.textContent = cardData.title ? cardData.title : '';
-    content.textContent = cardData.content ? cardData.content : '';
+    header.textContent = getTitle(cardData);
+    content.textContent = getDescription(cardData);
     var icon = document.createElement('img');
 
     IconsHelper.getIcon(cardData.url, 32, cardData).then(iconBlob => {
@@ -18,16 +71,16 @@
       container.appendChild(icon);
     });
 
-    if (cardData.screenshot || cardData.tile) {
-      var img = cardData.screenshot || cardData.tile;
-      var imgUrl = (typeof img === 'string') ? img : URL.createObjectURL(img);
-      container.style.backgroundImage = 'url(' + imgUrl + ')';
+    var backgroundURL = getBackgroundURL(cardData);
+    if (backgroundURL) {
+      container.style.backgroundImage = 'url(' + backgroundURL + ')';
     }
 
     if (cardData.url) {
       container.dataset.url = cardData.url;
     }
 
+    container.appendChild(background);
     container.appendChild(header);
     container.appendChild(content);
 
