e47e3d593a32bb40c7e413a736af17ef55ff4dd4
Sam Foster <sfoster@mozilla.com>
2016-09-23 15:18:43 -0700

    Populate pairNumber and testMode properties from mozSettings; stub in initiateCall for CallTest

--- apps/system/js/app.js
+++ apps/system/js/app.js
@@ -10,17 +10,19 @@
    *       from that number only
   */
   var loadTasks = [];
-  // Telephony object
-  var tel = navigator.mozTelephony;
 
   var App = function() {};
   App.prototype = {
     start: function() {
+      loadTasks.push(this.updatePairNumber());
+      loadTasks.push(this.updateTestMode());
+
       return Promise.all(loadTasks).then(() => {
         return this.started();
       });
     },
     testMode: 'emoji',
+    pairNumber: '',
     started: function() {
       if (this._started) {
         throw new Error('App: bootstrap should not be called twice.');
@@ -31,17 +33,68 @@
         document.body.dataset.testmode = this.testMode;
         document.body.dataset.ready = 'ready';
         console.log('bootstrapped, app is started');
-        console.log('Telephone object: ', tel);
         res(true);
       })];
 
+      console.log('starting in testMode: ', this.testMode);
       if (this.testMode == 'call') {
         this.testPanel = new exports.CallTest();
       } else if(this.testMode == 'emoji') {
         this.testPanel = new exports.EmojiTest();
+      } else {
+        throw new Error('Unexpected testMode: ' + this.testMode);
       }
       startedTasks.push(this.testPanel.start());
       return ;
+    },
+    _updateSetting: function(key, value) {
+      if (typeof value !== 'undefined') {
+        return new Promise((res, rej) => {
+          var req = navigator.mozSettings.createLock().set({ key: value });
+          req.onsuccess = () => {
+            console.log(key + ' updated to:', value);
+            res(value);
+          };
+          req.onerror = () => {
+            console.log(key + ' update failed:', req.error);
+            rej(false);
+          };
+        });
+      } else {
+        return new Promise((res, rej) => {
+          var req = navigator.mozSettings.createLock().get(key);
+          req.onsuccess = () => {
+            this.pairNumber = req.result[key];
+            res(req.result[key]);
+          };
+          req.onerror = () => {
+            console.log(key + ' get failed:', req.error);
+            rej(false);
+          };
+        });
+      }
+    },
+    updatePairNumber: function(num) {
+      return this._updateSetting('haiku.pair.number', num).then((res) => {
+        this.pairNumber = num;
+      });
+    },
+    updateTestMode: function(testMode) {
+      return this._updateSetting('haiku.testmode', testMode).then((res) => {
+        if (typeof testMode === 'undefined') {
+          console.log('settings get got: ', res);
+          // get, update our setting value doesnt agree with our current state
+          if (!res || this.testMode == res) {
+            console.log('no testMode change, it remains: ', this.testMode);
+            // no change
+          } else {
+            this.testMode = res;
+            console.log('updating testMode to: ', res);
+          }
+        } else {
+          this.testMode = testMode;
+        }
+      });
     }
   };
 
--- apps/system/js/call_test.js
+++ apps/system/js/call_test.js
@@ -3,17 +3,63 @@
   The Call-mode UI & functionality
 */
 (function(exports) {
+  // Telephony object
+  var tel = navigator.mozTelephony;
+
   var CallTest = function() {};
   CallTest.prototype = {
     start: function() {
       return new Promise((res, rej) => {
-        // hook up listeners etc
         console.log('CallTest starting');
+        // hook up listeners etc
+        tel.muted = false;
+        tel.speakerEnabled = true;
+
+        tel.ready.then((result) => {
+          console.log('Telephony is ready');
+        }, (err) => {
+          console.warn('Telephony error:', err.message);
+        });
+
         res(true);
       });
     },
     stop: function() {
       // unhook listeners etc
+    },
+    initiateCall: function() {
+      this._callInProgress = true;
+      var app = window.app;
+      var telNumber = app.pairNumber;
+      if (!telNumber) {
+        console.warn('no pair number configured');
+        return;
+      }
+      // Place a call
+      var call = tel.dial(telNumber).then(function(call) {
+        // Events for that call
+        call.onstatechange = function (event) {
+            /*
+                Possible values for state:
+                "dialing", "ringing", "busy", "connecting", "connected",
+                "disconnecting", "disconnected", "incoming"
+            */
+            console.log('call state change', event.state);
+        };
+
+        // Above options as direct events
+        call.onconnected = function () {
+            // Call was connected
+            console.log('call connected');
+        };
+
+        call.ondisconnected = function () {
+            console.log('call disconnected');
+        };
+      }, function(err) {
+        console.warn('Error trying to dial: ', err);
+      });
+      return call;
     }
   };
   exports.CallTest = CallTest;
